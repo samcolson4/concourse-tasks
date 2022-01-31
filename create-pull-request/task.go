5eb62952543cc779dee634b16f053c5b8813c8ce
Sam Colson <sam.colson@engineerbetter.com>
2022-01-31 17:10:20 +0000

    Add check for PR existing

--- create-pull-request/task.go
+++ create-pull-request/task.go
@@ -1,10 +1,25 @@
 package main
 
-import getPullRequests "github.com/samcolson4/concourse-tasks/create-pull-request/lib/github"
+import (
+	getPullRequests "github.com/samcolson4/concourse-tasks/create-pull-request/lib/github"
+	"fmt"
+)
 
 func main() {
-	ownerUsername := "engineerbetter"
-	repoName := "control-tower"
+	ownerUsername := "garethjevans"
+	repoName := "create-pr"
 
-	// allPrs := getPullRequests.GetAllPrs(ownerUsername, repoName)
+	allPrs := getPullRequests.GetAllPrs(ownerUsername, repoName)
+
+	// get dependency & version number that is being bumped to
+	dependency := "really-useful-tool"
+	newVersion := "0.3.2"
+
+	for _, pr := range allPrs {
+		pr_title := fmt.Sprintf("Bump %s to %s", dependency, newVersion)
+		if pr.Title == pr_title {
+			fmt.Println("Pull request already exists.")
+			break
+		}
+	}
 }
